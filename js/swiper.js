e90e6ab38c4a1e341d5e02e4d7ea8c486b3c1142
cjishy <146523683+cjishy@users.noreply.github.com>
2023-10-13 10:09:52 +0800

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -1,11 +1,11 @@
-var swiper = new Swiper('.swiper-container-lovexhj1', {
-    pagination: '.swiper-pagination-lovexhj1',
+var swiper = new Swiper('.swiper-container-loveshy1', {
+    pagination: '.swiper-pagination-loveshy1',
     paginationClickable: true,
     spaceBetween: 30,
 });
 
-var swiper = new Swiper('.swiper-container-lovexhj2', {
-    pagination: '.swiper-pagination-lovexhj2',
+var swiper = new Swiper('.swiper-container-loveshy2', {
+    pagination: '.swiper-pagination-loveshy2',
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
@@ -19,10 +19,10 @@ var swiper = new Swiper('.swiper-container-lovexhj2', {
     }
 });
 
-var swiper = new Swiper('.swiper-container-lovexhj3', {
-    pagination: '.swiper-pagination-lovexhj3',
+var swiper = new Swiper('.swiper-container-loveshy3', {
+    pagination: '.swiper-pagination-loveshy3',
     effect: 'flip',
     grabCursor: true,
-    nextButton: '.swiper-button-next-lovexhj3',
-    prevButton: '.swiper-button-prev-lovexhj3'
+    nextButton: '.swiper-button-next-loveshy3',
+    prevButton: '.swiper-button-prev-loveshy3'
 });
